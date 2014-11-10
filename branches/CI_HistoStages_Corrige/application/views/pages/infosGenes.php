837b733f58b25723bdf3c3bfa1fe8424cf45a229
annie.baraban@gmail.com <annie.baraban@gmail.com@cb4bdcaf-4b60-4f35-753f-d99343f55ba7>
2014-11-10 08:22:41 +0000

    Création de la branche corrigé de l'application CodeIgniter sur l'historique des stages.

---
+++ branches/CI_HistoStages_Corrige/application/views/pages/infosGenes.php
@@ -0,0 +1,16 @@
+  <!-- Division principale -->
+  <div id="contenu">
+      <p class="corpsTexte">
+      Ce site est mis à la disposition des étudiants et enseignants du BTS Services Informatiques aux Organisations du Lycée 
+      Victor et Hélène Basch - anciennement Ile de France - de Rennes. 
+      </p>
+      <p class="corpsTexte">
+      Ses objectifs:</p>
+      <ul class="corpsTexte">
+          <li>Informer sur le référentiel des activités professionnelles des deux parcours.</li>
+      
+          <li>Permettre aux étudiants d'accéder aux informations sur les stages passés.</li>
+      
+          <li>Faciliter les recherches de stage.</li>
+      </ul>
+  </div>
---
+++ branches/CI_HistoStages_Corrige/application/views/pages/licencesProGen.php
@@ -0,0 +1,76 @@
+  <!-- Division principale -->
+  <div id="contenu">
+    <h2>Les licences professionnelles</h2>
+      <ul>
+        <li>
+            Angers - Université d'Angers<br />
+            <a href="http://www.info.univ-angers.fr/info/enseignement/licence_pro.html">Logiciels Libres et propriétaires pour les systèmes réseaux et bases de données</a><br />
+        </li>
+        <li>
+		
+            Caen - Université de Caen<br />
+            <a href="http://www.info.unicaen.fr/diplome/LicPro">Activités et techniques de Communication, Webmestre</a><br />
+        </li>
+        <li>
+            Lannion - IUT<br />
+            <a href="http://www.iut-lannion.fr/choisir-une-licence-pro/"> Gestion des Systèmes et Réseaux / Intégration des Systèmes Voix et Données / Management Informatique et Commercial</a>
+        </li>
+        <li>
+            Laval - IUT<br />
+            <a href="http://www.iut-laval.univ-lemans.fr/fr/mmi-web-et-creation-multimedia/licence-pro-crspm.html">Conception et réalisation de services et produits multimédia</a> 
+        </li>
+        <li>
+            Laval - IUT<br />
+            <a href="http://www.iut-laval.univ-lemans.fr/fr/info-informatique-et-programmation/licence-pro-tql.html">Systèmes Informatiques et Logiciels - Spécialité Test et Qualité du Logiciel</a> 
+        </li>
+        <li>
+            Le Havre - IUT<br />
+            <a href="http://www.univ-lehavre.fr/iut/iut_lp_cand.htm#SICI">Systèmes d'Information et de Communication sur Internet</a>
+        </li>  
+        <li>
+            Nantes - IUT<br /> 
+            <a href="http://www.univ-nantes.fr/SI00165/0/fiche_917__formation/">Informatiques et Logiciels Spécialité : Développement d'applications réparties.</a>
+        </li>
+        <li>
+            Saint-Malo - IUT<br />
+            <a href="https://sfc.univ-rennes1.fr/informatique/lp_reseaux-securite.htm">Réseaux sans fil et sécurité des réseaux</a><br />
+            <a href="https://sfc.univ-rennes1.fr/informatique/lp_administration-securite-reseaux.htm">Administration et sécurité des réseaux</a>
+        </li>        
+        <li>
+            Vannes - IUT<br />
+            <a href="http://www.iu-vannes.fr/Formations/lpcsd/presentation.asp">
+            Licence Professionnelle Conception de Solutions Décisionnelles</a>
+        </li>
+        <li>
+            Vannes - IUT<br />
+            <a href="http://www.iu-vannes.fr/Formations/lps2im/presentation.asp">
+            Licence Professionnelle - Systèmes d'Information, intégration et maintenance</a>
+        </li>
+        <li>
+            Vannes - IUT<br />
+            <a href="http://www.iu-vannes.fr/Formations/lpsis/presentation.asp">
+            Licence Professionnelle - Statistique et Informatique pour la Santé</a>
+        </li>
+      </ul>
+    
+    <h2>Les licences générales</h2>
+      <ul>
+        <li>
+            Rennes - IFSIC - Licence informatique parcours MIAGE<br />
+            <a href="http://etudes.univ-rennes1.fr/miage">Licence générale - Parcours Méthodes Informatiques Appliquées à la Gestion des Entreprises</a>
+        </li>
+        <li>
+            Rennes - ESIR<br />
+            <a href="http://esir.univ-rennes1.fr/presentation">Ecole d'Ingénieurs de Rennes</a>
+        </li>
+        <li>
+            Brest - U.B.O.<br />
+            <a href="http://iup-info.univ-brest.fr/licences.php">Licence Ingénierie Informatique</a>
+        </li>
+        <li>
+            Nantes - Université de Nantes<br />
+            <a href="http://www.univ-nantes.fr/SI00136/0/fiche___formation/">Licence informatique</a>
+        </li>
+      </ul>
+</div>
+
