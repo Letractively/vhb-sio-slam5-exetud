f2bf27ad7d5d30b8cb596c336b9e1455c1895877
annie.baraban@gmail.com <annie.baraban@gmail.com@cb4bdcaf-4b60-4f35-753f-d99343f55ba7>
2014-11-16 20:21:22 +0000

    Corrigé TP étape 1 - Evolutions du contrôleur Accueil avec nouveau lien dans le menu et nouvelle vue. Ecriture d'une méthode privée dans le contrôleur.

--- branches/CI_HistoStages_Corrige/application/controllers/accueil.php
+++ branches/CI_HistoStages_Corrige/application/controllers/accueil.php
@@ -6,13 +6,38 @@
  */
 class Accueil extends CI_Controller {
     /**
-     * Prépare pour l'affichage les informations générales sur le site
+     * Prépare et demande l'affichage de la liste des études post-bts sio
+     */
+    public function licencesProGen() {
+        $this->traiterDemande("Licences professionnelles et générales", "licencesProGen");
+    }
+    /**
+     * Prépare et demande l'affichage de la liste de sites d'offres emplois et stages
+     */
+    public function sitesEmplois() {
+        $this->traiterDemande("Sites d'offres emplois et stages", "sitesEmplois");
+    }
+    /**
+     * Prépare et demande l'affichage des informations générales sur le site
      */
     public function infosGenes() {
-                $this->load->view('include/entete');
-                $this->load->view('include/sommaire');
-		$this->load->view('pages/infosGenes');
-                $this->load->view('include/pied');                
+        $this->traiterDemande("Informations générales", "infosGenes");
+    }
+    /**
+     * Prépare et demande l'affichage d'une page complète d'après un titre et un nom de page
+     * @param string $title
+     * @param string $page
+     */
+    private function traiterDemande($title, $page) {
+        $data['title']=$title;
+        $this->load->view('include/entete', $data);
+        $this->load->view('include/sommaire');
+        $this->load->view('pages/' . $page);
+        $this->load->view('include/pied');                
+        
+    }
+    public function index() {
+        $this->infosGenes();
     }
 }
 ?>
--- branches/CI_HistoStages_Corrige/application/views/include/sommaire.php
+++ branches/CI_HistoStages_Corrige/application/views/include/sommaire.php
@@ -4,6 +4,9 @@
            <li class="smenu">
               <a href="<?php echo site_url("Accueil/infosGenes"); ?>" title="Accueil">Accueil</a>
               <a href="<?php echo site_url("Accueil/sitesEmplois"); ?>" title="Sites web d'offres d'emplois et de stages">Sites emplois et stages</a>
+              <a href="<?php echo site_url("Accueil/licencesProGen"); ?>" title="Licences professionnelles et générales">Licences professionnelles et générales</a>
+              <a href="<?php echo site_url("gererOrganisations"); ?>" title="Liste des organisations">Organisations</a>
+              <a href="index.php/gererOrganisations" title="Liste des organisations">Organisations bis</a>
            </li>
         </ul>
     </div>       
